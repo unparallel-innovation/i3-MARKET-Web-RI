80fb91a9603e7ff2ad340aba656b8c0cb1cf2c11
Paulo Andre Azevedo Quirino <paulo.quirino@unparallel.pt>
2021-07-27 10:19:37 +0100

    search component testing for showing inputs while loading

--- pages/search/index.js
+++ pages/search/index.js
@@ -8,27 +8,9 @@ import {Form, Button, Row} from 'react-bootstrap'
 import {Loading} from "../../components/Loading";
 import OfferingCard from "../../components/offerings/OfferingCard";
 
-export default
-function Index() {
-  const router = useRouter();
-  const { searchType = "provider", providerId, category } = router.query;
-  const { data, error } = useData(
-    `/api/search?searchType=${searchType}&providerid=${providerId}&category=${category}`
-  );
+function Search(props) {
+  const { offerings, providers, categories, searchType } = props;
   const [ _searchType, setSearchType ] = useState(searchType);
-
-  if (error)
-    return <ErrorC error={error} />;
-
-  if (!data)
-    return <Loading />;
-
-
-  console.log(`Searching for ${searchType} provider ${providerId} category ${category}`);
-
-  let selectEl = null;
-
-  const { offerings, providers, categories} = data
 
   const providerEl = providers.map((item, idx) => (
       <option key={idx} value={item}>{item}</option>
@@ -38,46 +20,43 @@ function Index() {
       <option key={idx} value={item.name}>{item.name}</option>
   ));
 
-  const offeringsEl = offerings.map(offering => (
-      <OfferingCard key={offering.title} {...offering} />
-  ));
-
+  let selectEl = null;
 
   if (_searchType === "provider") {
     selectEl = (<Form.Control as="select" className="mr-3 dropdown-custom"
-      name="providerId" defaultValue={_searchType}>
+      name="providerId">
       { providerEl}
     </Form.Control>);
   }
 
   if (_searchType === "category") {
     selectEl = (<Form.Control as="select" className="mr-3 dropdown-custom"
-      name="category" defaultValue={category}
+      name="category"
     >
       { categoriesEl }
     </Form.Control>);
   }
 
+  const offeringsEl = offerings ? offerings.map(offering => (
+      <OfferingCard key={offering.title} {...offering} />
+  )) : <Col>Search for offerings</Col>;
 
-  return (<Layout>
-    <div className="px-5">
-      <Form className="d-inline-flex mb-5" onSubmit={onSubmit}>
-        <Form.Control as="select" onChange={onChange} className="mr-3 bg-primary text-white dropdown-custom"
-                      name="searchType" value={_searchType}
-        >
-          <option value="provider">Provider</option>
-          <option value="category">Category</option>
-        </Form.Control>
-        { selectEl }
-        <Button type="submit">Search</Button>
-      </Form>
-      <Row>
-        { offeringsEl }
-      </Row>
+  return (<div>
+    <Form className="d-inline-flex mb-5" onSubmit={onSubmit}>
+      <Form.Control as="select" onChange={onChange} className="mr-3 bg-primary text-white dropdown-custom"
+        name="searchType" value={_searchType}
+      >
+        <option value="provider">Provider</option>
+        <option value="category">Category</option>
+      </Form.Control>
+      { selectEl }
+      <Button type="submit">Search</Button>
+    </Form>
 
-    </div>
-  </Layout>);
-
+    <Row>
+      { offeringsEl }
+    </Row>
+  </div>);
 
   function onSubmit(e) {
     e.preventDefault();
@@ -92,6 +71,36 @@ function Index() {
   function onChange(e) {
     setSearchType(e.target.value);
   }
+}
+
+export default
+function Index() {
+  const router = useRouter();
+  const { searchType = "provider", providerId, category } = router.query;
+  const { data, error } = useData(
+    `/api/search?searchType=${searchType}&providerid=${providerId}&category=${category}`
+  );
+  // const [ _searchType, setSearchType ] = useState(searchType);
+
+  if (error)
+    return <ErrorC error={error} />;
+
+  if (!data)
+    return (<Layout>
+      <div className="px-5">
+        <Search offerings={[]} providers={[]} categories={[]} searchType={searchType} />
+      </div>
+    </Layout>);
+
+  console.log(`Searching for ${searchType} provider ${providerId} category ${category}`, data);
+
+  const { offerings, providers, categories} = data
+
+  return (<Layout>
+    <div className="px-5">
+      <Search { ...data } searchType={searchType} />
+    </div>
+  </Layout>);
 
   // return (<Layout>
   //   <div className="px-5">

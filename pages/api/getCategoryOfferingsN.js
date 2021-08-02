d4fe40a7b45e4cdcc4c892f8ddeb4d13c377e7f7
Paulo Andre Azevedo Quirino <paulo.quirino@unparallel.pt>
2021-08-02 10:48:06 +0100

    hardcoded number of offerings in category

--- pages/api/getCategoryOfferingsN.js
+++ pages/api/getCategoryOfferingsN.js
@@ -3,10 +3,10 @@ import { connector } from '/lib/server.js'
 export default async function handler(req, res) {
     const { category } = req.query;
 
-    const offerings = await connector.getCategoryOfferings(category);
+    // const offerings = await connector.getCategoryOfferings(category);
 
     res.status(200).json({
-      offeringsN: offerings.length,
-      // offeringsN: 3,
+      // offeringsN: offerings.length,
+      offeringsN: 3,
     });
 }

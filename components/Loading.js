f11944695d0e2fd7dcf77a43958db034a3a745ad
Pedro Ferreira <pedro.ferreira@unparallel.pt>
2021-08-03 10:08:48 +0100

    show error message when api fetch fails; update loading message

--- components/Loading.js
+++ components/Loading.js
@@ -2,8 +2,8 @@ import {Layout} from "./common";
 
 export function Loading() {
     return (<Layout>
-        <div className="px-5 py-3">
-            Loading...
+        <div className="d-flex w-100 flex-grow-1 justify-content-center align-items-center h3 text-lightgray">
+            Loading results.. Please wait..
         </div>
     </Layout>);
 }
--- pages/transactions/index.js
+++ pages/transactions/index.js
@@ -4,7 +4,7 @@ export default function Transactions() {
 
     return (
         <Layout>
-            <div className="d-flex w-100 h-100 justify-content-center align-items-center h3 text-lightgray">
+            <div className="d-flex w-100 justify-content-center align-items-center h3 text-lightgray">
                 Coming soon
             </div>
         </Layout>

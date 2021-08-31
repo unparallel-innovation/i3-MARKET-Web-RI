5dc869297620801458e4b1cae05c829a2ec7424c
Paulo Andre Azevedo Quirino <paulo.quirino@unparallel.pt>
2021-08-31 16:40:26 +0100

    useUser allow noRedirect. Layout does not redirect for 404 page. noBreadcrumbs prop

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,11 @@
-import Layout404 from '/components/Layout404.js';
+import Layout from '/components/Layout.js';
 
 export default function Custom404Page() {
     return (
-        <Layout404>
+        <Layout className="d-flex" noRedirect noBreadcrumbs>
             <div className="px-5 d-flex w-100 justify-content-center align-items-center h3 text-lightgray">
                 404 - Page not found
             </div>
-        </Layout404>
+        </Layout>
     );
 }

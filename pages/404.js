dfdf670fa070c0c8979863e63fc300006900905a
Pedro Ferreira <pedro.ferreira@unparallel.pt>
2021-08-31 16:02:38 +0100

    testing layout for 404 pages

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,11 @@
-import Layout from '/components/Layout.js';
+import Layout404 from '/components/Layout404.js';
 
 export default function Custom404Page() {
     return (
-        <Layout>
+        <Layout404>
             <div className="px-5 d-flex w-100 justify-content-center align-items-center h3 text-lightgray">
                 404 - Page not found
             </div>
-        </Layout>
+        </Layout404>
     );
 }

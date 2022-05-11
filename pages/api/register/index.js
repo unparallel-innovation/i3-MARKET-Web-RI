086ae15f4db831cc576887016fc399a66076f8a0
Pedro Ferreira <pedro.ferreira@unparallel.pt>
2022-05-11 13:57:45 +0100

    add verifiable credential env variable; update oidc issuer configuration

--- pages/api/register/index.js
+++ pages/api/register/index.js
@@ -4,7 +4,7 @@ export default catchErrors(async (req, res) => {
     switch (req.method) {
         case 'POST':
             const credential = req.body;
-            const ic_url = connector.getIssueCredentialUrl(process.env.OIDC_URL, credential, `${req.headers.origin}/login`);
+            const ic_url = connector.getIssueCredentialUrl(process.env.VC_URL, credential, `${req.headers.origin}/login`);
             return {url: ic_url};
     }
 });

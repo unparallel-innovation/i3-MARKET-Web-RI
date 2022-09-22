f816af372ada7ec939f672fb9540b707764c68cf
Pedro Ferreira <pedro.ferreira@unparallel.pt>
2022-09-22 15:47:49 +0100

    show dataExchangeSpec in offering page

--- components/offering/details/Distribution.js
+++ components/offering/details/Distribution.js
@@ -6,7 +6,8 @@ export default
 function Distribution(props) {
     const {
         title, description, license, accessRights, downloadType,
-        conformsTo, mediaType, packageFormat, dataStream, accessService, eventKey
+        conformsTo, mediaType, packageFormat, dataStream,
+        accessService, dataExchangeSpec, eventKey
     } = props;
 
     return (
@@ -56,23 +57,51 @@ function Distribution(props) {
 
                         <Table className="mt-3">
                             <thead>
-                                <tr>
-                                    <th>Conforms To</th>
-                                    <th>Endpoint Description</th>
-                                    <th>Endpoint URL</th>
-                                    <th>Serves Dataset</th>
-                                    <th>Service Specs</th>
-                                </tr>
+                            <tr>
+                                <th>Conforms To</th>
+                                <th>Endpoint Description</th>
+                                <th>Endpoint URL</th>
+                                <th>Serves Dataset</th>
+                                <th>Service Specs</th>
+                            </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>{ accessService.conformsTo }</td>
-                                    <td>{ accessService.endpointDescription }</td>
-                                    <td>{ accessService.endpointURL }</td>
-                                    <td>{ accessService.servesDataset }</td>
-                                    <td>{ accessService.serviceSpecs }</td>
-                                </tr>
+                            <tr>
+                                <td>{ accessService.conformsTo }</td>
+                                <td>{ accessService.endpointDescription }</td>
+                                <td>{ accessService.endpointURL }</td>
+                                <td>{ accessService.servesDataset }</td>
+                                <td>{ accessService.serviceSpecs }</td>
+                            </tr>
                             </tbody>
+                        </Table>
+
+                        <h5 className="text-center mt-4 text-dark">
+                            Data Exchange Spec
+                        </h5>
+
+                        <Table className="mt-3">
+                            <thead>
+                            <tr>
+                                <th>Encryption Algorithm</th>
+                                <th>Signing Algorithm</th>
+                                <th>Hash Algorithm</th>
+                                <th>PoO to PoR delay</th>
+                                <th>PoO to PoP delay</th>
+                                <th>PoO to Secret delay</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>{ dataExchangeSpec.encAlg }</td>
+                                <td>{ dataExchangeSpec.signingAlg }</td>
+                                <td>{ dataExchangeSpec.hashAlg }</td>
+                                <td>{ dataExchangeSpec.pooToPorDelay }</td>
+                                <td>{ dataExchangeSpec.pooToPorDelay }</td>
+                                <td>{ dataExchangeSpec.pooToSecretDelay }</td>
+                            </tr>
+                            </tbody>
+
                         </Table>
                     </Card.Body>
                 </Accordion.Collapse>
